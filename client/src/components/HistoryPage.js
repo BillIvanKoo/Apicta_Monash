02f8c4fb4adcf0f33695adde888a074b60a0d757
Bill Ivan Kooslarto <kooslarto.bill.ivan@gmail.com>
2019-08-25 18:08:13 +0800

    add feature to get packets data in the selected date and hour range

--- client/src/components/HistoryPage.js
+++ client/src/components/HistoryPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Form, InputGroup, DropdownButton, ButtonGroup, Button, Dropdown } from 'react-bootstrap';
 import axios from 'axios';
 import { Pie } from 'react-chartjs-2';
 
@@ -12,7 +12,7 @@ class HistoryPage extends Component {
         super(props)
         this.state= {
             url: Constant.URL,
-            hour: 1566593922,
+            dateHour: 1566593922,
             packets: [],
             activePacket: {
               port_src: [],
@@ -57,48 +57,34 @@ class HistoryPage extends Component {
                         Constant.COLOR2
                     ]
                 }]
-            }
+            },
+            displayedDate: "",
+            selectedView: "Total",
+            inputDateValue: "",
+            selectedHour: ""
         }
     }
 
     componentDidMount() {
-        console.log(this.props.location)
         let that = this
         if (this.props.location.data) {
             axios.get(`${this.state.url}/packets/hour/${this.props.location.data.timestamp}`).then(res => {
-                console.log(res)
                 that.setState({
-                    packets: res.data.reverse(),
-
-                    // pieTotal: {
-                    //     ...that.state.pieTotal,
-                    //     datasets: [{
-                    //         ...that.state.pieTotal.datasets,
-                    //         data: [that.props.location.data[`total_tcp`], that.props.location.data[`total_udp`]]
-                    //     }]
-                    // },
-                    // pieSize: {
-                    //     ...that.state.pieSize,
-                    //     datasets: [{
-                    //         ...that.state.pieSize.datasets,
-                    //         data: [that.props.location.data[`size_tcp`], that.props.location.data[`size_udp`]]
-                    //     }]
-                    // },
-                }, () => { this.handleLineClick(this.state.packets.findIndex(({id}) => id === this.props.location.data.id)) })
+                    packets: res.data.sort((a,b)=>{
+                        let x = b.timestamp; let y = a.timestamp;
+                        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+                    }),
+                }, () => { that.handleLineClick(that.state.packets.findIndex(({id}) => id === this.props.location.data.id)) })
             })
         } else {
-            axios.get(`${this.state.url}/packets/hour/${this.state.hour}`).then(res => {
-                let packets = res.data.reverse()
-                that.setState({
-                    packets
-                }, () => { this.handleLineClick(this.state.packets.length-1) })
-            })
+            this.handleSubmit()
         }
     }
 
     handleLineClick(i){
         let that = this
         axios.get(`${this.state.url}/packets/${this.state.packets[i].id}`).then(res => {
+            let date = new Date(res.data.timestamp * 1000)
             that.setState({
                 activePacket: res.data,
                 pieTotal: {
@@ -115,7 +101,50 @@ class HistoryPage extends Component {
                         data: [res.data[`size_tcp`], res.data[`size_udp`]]
                     }]
                 },
+                displayedDate: date.toLocaleString(),
+                inputDateValue: `${date.getFullYear()}-${(date.getMonth()+1 < 10 ? "0" : "") + (date.getMonth()+1)}-${(date.getDate() < 10 ? "0" : "") + date.getDate()}`,
+                selectedHour: this.getHourRange(date.getHours())
             })
+        })
+    }
+
+    getHourRange(startHour){
+        return `${startHour<10?"0"+startHour:startHour}:00 - ${startHour + 1 === 24 ? "00" : ((startHour+1)<10?"0"+(startHour+1):(startHour+1))}:00`
+    }
+
+    handleSubmit(){
+        let that = this
+        axios.get(`${this.state.url}/packets/hour/${this.state.dateHour}`).then(res => {
+            console.log(res.data);
+            let packets = res.data.sort((a,b)=>{
+                let x = b.timestamp; let y = a.timestamp;
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            })
+            that.setState({
+                packets
+            }, () => { that.handleLineClick(that.state.packets.length-1) })
+        })
+    }
+
+    handleDateChange(dateString) {
+        this.setState({
+            inputDateValue: dateString
+        }, this.updateDateHour)
+    }
+
+    handleHourSelect(hour){
+        this.setState({
+            selectedHour: this.getHourRange(hour),
+        }, this.updateDateHour)
+    }
+
+    updateDateHour() {
+        let date = new Date(this.state.inputDateValue)
+        let unixDate = Math.floor(date.getTime()/1000)
+        unixDate += date.getTimezoneOffset() * 60
+        unixDate += (parseInt(this.state.selectedHour.substr(0,2))) * 3600
+        return this.setState({
+            dateHour: unixDate
         })
     }
 
@@ -123,7 +152,53 @@ class HistoryPage extends Component {
         return (
             <Container fluid>
                 <Row>
-
+                    <Col md={8}>
+                        <Form>
+                            
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>
+                                            Date and Hour
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <Form.Control
+                                        type="date"
+                                        value={this.state.inputDateValue}
+                                        onChange={(e)=>{this.setState({inputDateValue: e.target.value})}}
+                                    />
+                                    <Dropdown
+                                        as={InputGroup.Append}
+                                    >
+                                        <Dropdown.Toggle>
+                                            {this.state.selectedHour}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu style={{height: "20vh", overflowY: "scroll"}}>
+                                            {[...Array(24).keys()].map(i => {
+                                                return (
+                                                    <Dropdown.Item key={i} onSelect={()=>{this.handleHourSelect(i)}}>
+                                                        {this.getHourRange(i)}
+                                                    </Dropdown.Item>
+                                                )
+                                            })}
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                    <ButtonGroup as={InputGroup.Append}>
+                                        <Button onClick={()=>{this.handleSubmit()}}>Submit</Button>
+                                    </ButtonGroup>
+                                    <DropdownButton
+                                    as={InputGroup.Append}
+                                    title={this.state.selectedView}
+                                    >
+                                        <Dropdown.Item onSelect={()=>{this.setState({selectedView: "Total"})}}>Total Number</Dropdown.Item>
+                                        <Dropdown.Item onSelect={()=>{this.setState({selectedView: "Size"})}}>Total Size</Dropdown.Item>
+                                    </DropdownButton>
+                                </InputGroup>
+                            
+                        </Form>
+                    </Col>
+                    <Col md={4}>
+                        Current displayed date and hour: {this.state.displayedDate}
+                    </Col>
                 </Row>
                 <Row>
                     <Col md={8} style={{maxHeight: "70vh"}}>
@@ -131,7 +206,7 @@ class HistoryPage extends Component {
                             <TotalSizeLine
                                 style={{height: "70vh"}}
                                 packetList={this.state.packets}
-                                name="Total"
+                                name={this.state.selectedView}
                                 handleClick={i=>{this.handleLineClick(i)}}
                             />
                         </Card>
